761fdf1bf575762bc157b03a7239529c3a8bbf84
Murali-P <murali.p@huawei.com>
2017-02-01 14:07:26 +0530

    Modify config files
    Resolved:INT-77
    Docker image creation
    
    Change-Id: I5c27631fd40dcc7eea56fe8f9c9bb493a7781c25
    Signed-off-by: Murali-P <murali.p@huawei.com>

--- distribution/standalone/src/main/assembly/run.sh
+++ distribution/standalone/src/main/assembly/run.sh
@@ -1,19 +1,14 @@
-
 DIRNAME=`dirname $0`
 RUNHOME=`cd $DIRNAME/; pwd`
 echo @RUNHOME@ $RUNHOME
-
 echo @JAVA_HOME@ $JAVA_HOME
 JAVA="$JAVA_HOME/bin/java"
 echo @JAVA@ $JAVA
-
 JAVA_OPTS="-Xms50m -Xmx128m"
-port=8312
+port=8701
 #JAVA_OPTS="$JAVA_OPTS -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=$port,server=y,suspend=n"
 echo @JAVA_OPTS@ $JAVA_OPTS
-
-class_path="$RUNHOME/:$RUNHOME/org.openo.vnfsdk.functest.jar"
-echo @class_path@ $class_path
-
-"$JAVA" $JAVA_OPTS -classpath "$class_path" org.openo.vnfsdk.functest.VnfSdkFuncTestApp server "$RUNHOME/conf/vnfsdkfunctest.yml"
+export CLASSPATH=CLASSPATH:"$JAVA_HOME/jre/lib"
+echo @CLASSPATH@ $CLASSPATH
+"$JAVA" $JAVA_OPTS -cp $RUNHOME/vnf-sdk-function-test.jar  org.openo.vnfsdk.functest.VnfSdkFuncTestApp server "$RUNHOME/conf/vnfsdkfunctest.yml"
 
